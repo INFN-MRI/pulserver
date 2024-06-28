5acf892aadbaa7f40f723adfdc548739871f15b8
Matteo Cencini <matteo.cencini@gmail.com>
2024-06-28 16:57:09 +0200

    add common readout blocks templates.

---
+++ src/pulseforge/_readout/line_readout.py
@@ -0,0 +1,6 @@
+"""Line readout creation subroutines."""
+
+import pypulseq as pp
+
+def make_line_readout(fov, npix, osf, sys):
+    pass
---
+++ src/pulseforge/_readout/rosette_readout.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 13:55:23 2024
+
+@author: mcencini
+"""
+
