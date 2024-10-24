fix: wrong units in _make_spoiler_grad

src/pulserver/_core/_ceq.py
@@ -36,7 +36,6 @@ class PulseqShapeArbitrary:
         # determine amplitude and normalize waveform
         self.amplitude = abs(self.magnitude).max()
         self.magnitude /= self.amplitude
-        self.magnitude *= 32767
 
     def to_bytes(self, endian=">") -> bytes:
         _bytes = struct.pack(endian + "i", self.n_samples) + struct.pack(
@@ -79,6 +78,7 @@ class PulseqShapeTrap:
 @dataclass
 class PulseqRF:
     type: int
+    complexflag: int
     wav: PulseqShapeArbitrary
     duration: float
     delay: float
@@ -86,6 +86,7 @@ class PulseqRF:
     def to_bytes(self, endian=">") -> bytes:
         return (
             struct.pack(endian + "h", self.type)
+            + struct.pack(endian + "h", self.complexflag)
             + self.wav.to_bytes(endian)
             + struct.pack(endian + "f", self.duration)
             + struct.pack(endian + "f", self.delay)
@@ -107,11 +108,21 @@ class PulseqRF:
             raster = 0.0
             time = data.t
 
-        rho = np.abs(data.signal)
-        theta = np.angle(data.signal)
+        sum_rf_real = sum(abs(data.signal.real))
+        sum_rf_imag = sum(abs(data.signal.imag))
+
+        if sum_rf_real > 100 * sum_rf_imag:
+            complexflag = 0
+            rho = data.signal.real
+            theta = None
+        else:
+            complexflag = 1
+            rho = np.abs(data.signal)
+            theta = np.angle(data.signal)
 
         return cls(
             type=type,
+            complexflag=complexflag,
             wav=PulseqShapeArbitrary(n_samples, raster, time, rho, theta),
             duration=data.shape_dur,
             delay=data.delay,

src/pulserver/blocks/_misc/_make_spoiler_gradient.py
@@ -47,18 +47,17 @@ def make_spoiler_gradient(
     if channel not in ["x", "y", "z"]:
         raise ValueError(f"Unrecognized channel {channel} - must be 'x', 'y', or 'z'.")
 
-    # # k space area
-    # dr = fov / npix
-
+    dr = voxel_size * 1e-3 # mm -> m
+    
     # prepare phase encoding gradient lobe
     if duration:
         return pp.make_trapezoid(
             channel=channel,
-            area=(ncycles * np.pi / voxel_size),
+            area=(ncycles * np.pi / dr),
             system=system,
             duration=duration,
         )
 
     return pp.make_trapezoid(
-        channel=channel, area=(ncycles * np.pi / voxel_size), system=system
+        channel=channel, area=(ncycles * np.pi / dr), system=system
     )